a7333b9a9f35a51bc13318839addf9281afc7d91
sonhaxp <sonhaxp@gmail.com>
2022-06-28 23:30:53 +0700

    first commit

---
+++ Gara-main/Garage Management System/Garage Management System/Connection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Garage_Management_System
+{
+    class Connection
+    {
+        public String db = @"Data Source=localhost;Initial Catalog=GARAGE;Integrated Security=True";
+    }
+}
